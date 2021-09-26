e2eed398a785739d2e87bd3f2f770a483eabefe6
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2021-09-26 21:51:13 +0100

    finished exercises with simple IFs

--- progai_week1.py
+++ progai_week1.py
@@ -6,7 +6,27 @@ Created on Sun Sep 26 21:10:13 2021
 """
 
 def is_odd(your_favorite_number):
-    print(your_favorite_number % 2 == 0)
+    if your_favorite_number % 2 == 0:
+        print('even')
+    else: 
+        print('odd')
+    
+    
+
+def is_negative(your_number):  
+    response = 'negative' if your_number < 0 else 'positive'
+    print(response)    
+    
+
+def is_negative_obscure_way(your_number):  
+    response = ('negative[2]', 'positive[2]')[your_number >= 0]
+    print(response)        
+    
+    
+# demo
     
 any_number = int(input('Enter a integer: '))
-print(is_odd(any_number))
+is_odd(any_number)
+is_negative(any_number)
+is_negative_obscure_way(any_number)
+    
