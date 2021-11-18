d7020f5aa64ee7bd6ed79d24f3128d3b87056286
Victor Ferreira Silva <v.ferreira@psytechdigital.com>
2021-11-18 23:10:10 +0000

    adjusted structure folder

---
+++ StratThink/twitter/api_twitter.py
@@ -0,0 +1,46 @@
+import requests, json, yaml
+
+
+
+def get_bearer_token():
+    with open('config.yml') as f:    
+        config_vars = yaml.safe_load(f)
+        
+    return config_vars['bearer_token']
+        
+
+
+def request_tweet(tweet_id):    
+    '''
+    source: https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference
+    '''
+    
+    bearer = get_bearer_token()
+    headers = {'Authorization': f'Bearer {bearer}'}
+    
+    api_base_url = "https://api.twitter.com/2"
+    uri = f'{api_base_url}/tweets/{tweet_id}' # a specific tweet
+    
+    print(f'GET {uri} ...')
+    response = requests.get(uri, headers=headers)
+    
+    print(f'Status Code: {response.status_code}')    
+    response_json = response.json() 
+    
+    return response_json
+
+
+
+
+tweet = request_tweet(1461460720445440013)  
+
+print(tweet)   
+    
+
+
+    
+
+
+
+
+
---
+++ StratThink/yfinance/save_datasets.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+
+company = "GME"
+interval = "60m"
+start = "2019-11-20"
+end = "2021-11-17"
+file_name = f'datasets/{company}_{interval}_{start}_{end}.csv'
+
+
+try:
+    csv_file = open(file_name, "r")    
+    csv_file.close()
+    print(f'File \'{file_name}\' already exists')    
+except(FileNotFoundError):
+    print(f'Saving {file_name}...')
+    
+    data = yf.download(company, interval=interval, start=start, end=end)
+    csv_file = open(file_name, "w")    
+    csv_file.write(data.to_csv(line_terminator='\n'))
+    csv_file.close()
